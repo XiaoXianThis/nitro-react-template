f7c9cd7b9dd11573e64f512cc6d1f1d3c1372dec
XiaoXianThis <xiaoxianthis@outlook.com>
2026-04-06 03:17:54 +0800

    add PORT to .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import Pages from "vite-plugin-pages";
 import { nitro } from "nitro/vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    Pages({ dirs: ["app/pages"] }),
-    nitro(),
-  ],
-  resolve: {
-    tsconfigPaths: true
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  const port = Number(env.PORT || 5000);
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      Pages({ dirs: ["app/pages"] }),
+      nitro(),
+    ],
+    server: {
+      port,
+    },
+    preview: {
+      port,
+    },
+    resolve: {
+      tsconfigPaths: true,
+    },
+  };
 });
